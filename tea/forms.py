f9dc9888e01383efe83ec6b0848b8bec5c1affc1
mizuki sato <mizki.sato.0025@gmail.com>
2025-11-22 17:00:43 +0900

    send valification email for sign up

--- tea/forms.py
+++ tea/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 from .models import TeaReview
 
 
@@ -9,79 +10,101 @@ from .models import TeaReview
 User = get_user_model()
 
 
-class GeneralUserRegistrationForm(UserCreationForm):
-    """一般ユーザー登録フォーム（usernameなし）"""
+class GeneralUserRegistrationForm(forms.Form):
+    """一般ユーザー登録フォーム（ModelFormを使わない）"""
     
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(attrs={
             'class': 'form-control',
             'placeholder': 'メールアドレス'
-        })
+        }),
+        label='メールアドレス'
     )
     
     nickname = forms.CharField(
         max_length=30,
-        required=True,
+        required=False,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'ニックネーム'
-        })
+            'placeholder': 'ニックネーム（任意）'
+        }),
+        label='ニックネーム'
     )
     
     password1 = forms.CharField(
-        label="パスワード",
+        label='パスワード',
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
             'placeholder': 'パスワード'
-        })
+        }),
+        help_text='8文字以上で、数字のみは使用できません。'
     )
     
     password2 = forms.CharField(
-        label="パスワード（確認）",
+        label='パスワード（確認）',
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
             'placeholder': 'パスワード（確認）'
-        })
+        }),
+        help_text='確認のため、同じパスワードを入力してください。'
     )
     
-    class Meta:
-        model = User
-        fields = ['email', 'nickname', 'password1', 'password2']
-    
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.nickname = self.cleaned_data['nickname']
-        # 一般ユーザーなのでusernameは自動生成される
-        user.is_staff = False
-        user.is_superuser = False
+    def clean_password1(self):
+        """パスワードのバリデーション"""
+        password1 = self.cleaned_data.get('password1')
         
-        if commit:
-            user.save()
-        return user
+        # Djangoのパスワードバリデーターを実行
+        try:
+            validate_password(password1)
+        except ValidationError as e:
+            raise ValidationError(e.messages)
+        
+        return password1
+    
+    def clean(self):
+        """パスワード一致確認"""
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        
+        if password1 and password2 and password1 != password2:
+            raise ValidationError({
+                'password2': 'パスワードが一致しません。'
+            })
+        
+        return cleaned_data
 
 
 class EmailAuthenticationForm(AuthenticationForm):
-    """メールアドレスでログインするフォーム"""
+    """メールアドレス認証フォーム"""
     
     username = forms.EmailField(
-        label="メールアドレス",
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'メールアドレス',
-            'autofocus': True
-        })
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'メールアドレス'}),
+        label='メールアドレス'
     )
     
     password = forms.CharField(
-        label="パスワード",
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'パスワード'
-        })
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'パスワード'}),
+        label='パスワード'
     )
-
+    
+    def confirm_login_allowed(self, user):
+        """ログイン可否の確認"""
+        # まず親クラスのチェック(is_activeなど)
+        super().confirm_login_allowed(user)
+        
+        # スーパーユーザーとスタッフはメール確認不要
+        if user.is_superuser or user.is_staff:
+            return
+        
+        # 一般ユーザーはメール確認必須
+        if not user.is_email_verified:
+            raise ValidationError(
+                'メールアドレスの確認が完了していません。'
+                '登録時に送信されたメール内のリンクをクリックしてください。',
+                code='email_not_verified',
+            )
 
 class ReviewForm(forms.ModelForm):
     """レビューフォーム"""
